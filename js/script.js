659f4eac9da4a3d0874574057b7d6115b404f607
Lena <olena.ratushna21@gmail.com>
2021-08-30 16:44:50 +0300

    Changed countdown() function

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ const newSpring = '1 Mar 2022';
 const daysElement = document.getElementById('days'),
       hoursElement = document.getElementById('hours'),
       minsElement = document.getElementById('mins'),
-      secondsElement = document.getElementById('seconds');
+      secondsElement = document.getElementById('seconds'),
+      timeInterval = setInterval(countdown,1000);
 
 function countdown(){
     const newSpringDate = new Date(newSpring);
     const currentDate = new Date();
 
     const totalSeconds = (newSpringDate - currentDate) / 1000;
+
+    if(totalSeconds <= 0 ) {
+        clearInterval(timeInterval);
+        document.querySelector('.countdown').innerHTML = '<h1 class="countdown__title">Spring is here already</h1>';
+    }
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
@@ -28,5 +34,3 @@ function formatTime(time) {
 
 //initial call
 countdown();
-
-setInterval(countdown,1000);
